9eec3b1b968fb589d750fb225f12abb0c8808af5
danielcsulb <danielmendia86@gmail.com>
2017-10-12 21:32:21 -0700

    comit

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const message = prompt('Enter a message')
+const message = prompt('Enter a number between 2 and 10:')
 
 // alert('Your input is: ' + message)
-const output = document.getElementById("output")
-output.innerHTML = 'Your message is ' + message
+
+if (message == 6) {
+    const output = document.getElementById("output")
+    output.innerHTML = 'Your input number is ' + message
+}
+break
+if (11 <= message <= 1) {
+    const output = document.getElementById("output")
+    output.innerHTML = 'Your input is ' + message + '. The valid input is between 2 and 10. Please reload this page again.'
+}
+break
