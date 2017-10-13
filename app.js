84733878c7167e6d13fe34cb6fe388e387c284f6
danielcsulb <danielmendia86@gmail.com>
2017-10-12 22:03:57 -0700

    comit

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const message = prompt('Enter a number between 2 and 10:')
 // alert('Your input is: ' + message)
 
 if (message == 6) {
-    const output = document.getElementById("output")
-    output.innerHTML = 'Your input number is ' + message
-}
-break
-if (11 <= message <= 1) {
-    const output = document.getElementById("output")
-    output.innerHTML = 'Your input is ' + message + '. The valid input is between 2 and 10. Please reload this page again.'
-}
-break
+const output = document.getElementById("output")
+output.innerHTML = 'Your input number  is ' + message };
+
+    if ( (message <= 1) || (message >= 11)) {
+        const output = document.getElementById("output")
+        output.innerHTML = 'Your input is ' + message + '. The valid input number is between 2 and 10. Please reload this page and try again.'
+        }
+    
