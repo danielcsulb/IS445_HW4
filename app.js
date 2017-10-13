c47bb4164b0b95429eb6d29c736204a6dd2eb564
danielcsulb <danielmendia86@gmail.com>
2017-10-13 01:34:33 -0700

    math

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const message = prompt('Enter a number between 2 and 10:')
 
+
 // alert('Your input is: ' + message)
 
 
@@ -8,9 +9,16 @@ if (message >= 2 && message <= 10) {
     const output = document.getElementById("output")
     output.innerHTML = 'Your input number  is ' + message;
 
+    n=message;
+    counter=0;
+    while (n > .000001 ) {
+        n /= 2;
+        counter ++;
+    }
+
 
     const output2 = document.getElementById("output2")
-    output2.innerHTML = 'The number of times to divide the number  ' + message + ' by 2 to get a value lower than one millionth is ';
+    output2.innerHTML = 'The number of times to divide the number  ' + message + ' by 2 to get a value lower than one millionth is '+ counter;
        
 }
 
