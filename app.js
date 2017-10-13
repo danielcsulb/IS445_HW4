c5e1a16ff7193531f42daa858656b6ff16ed212e
danielcsulb <danielmendia86@gmail.com>
2017-10-13 01:17:12 -0700

    comit

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const message = prompt('Enter a number between 2 and 10:')
 
 // alert('Your input is: ' + message)
 
-if (message == 6) {
-const output = document.getElementById("output")
-output.innerHTML = 'Your input number  is ' + message };
 
-    if ( (message <= 1) || (message >= 11)) {
-        const output = document.getElementById("output")
-        output.innerHTML = 'Your input is ' + message + '. The valid input number is between 2 and 10. Please reload this page and try again.'
-        }
+if (message >= 2 && message <= 10) {
+
+    const output = document.getElementById("output")
+    output.innerHTML = 'Your input number  is ' + message;
+
+
+    const output2 = document.getElementById("output2")
+    output2.innerHTML = 'The number of times to divide the number  ' + message + ' by 2 to get a value lower than one millionth is ';
+       
+}
+
+
+// if ((message > 1) || (message < 11)) {
+// const output2 = document.getElementById("output2")
+// output2.innerHTML = 'The number of time to divide the number ' + message + ' by 2 to get a value less than one millionth is ' 
+// }
+
+if ( message <= 1 || message >= 11) {
+    const output = document.getElementById("output")
+    output.innerHTML = 'Your input is ' + message + '. The valid input number is between 2 and 10. Please reload this page and try again.'
+    }
+
+
     
