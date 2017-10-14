30ea5278f5131ef8dddf59bb4c2c427e8853e86e
danielcsulb <danielmendia86@gmail.com>
2017-10-13 23:08:22 -0700

    comit

--- app.js
+++ app.js
@@ -19,11 +19,34 @@ if (message >= 2 && message <= 10) {
 
     const output2 = document.getElementById("output2")
     output2.innerHTML = 'The number of times to divide the number  ' + message + ' by 2 to get a value lower than one millionth is '+ counter;
+   
 
-    div.innerHTML = message * '*'
-       
+  function pattern1(){
+    //var i;
+    var i = message;
+    message=i;
+       for (var i = message; i >0 ; i--)
+       {
+           for (var j=message; j<= i; j++)
+            {
+             //console.log("* ");
+             const pattern = document.getElementById("pattern")
+             pattern.innerHTML = "* ";   
+            }
+            //console.log("\n");
+            const pattern = document.getElementById("pattern")
+            pattern.innerHTML = "\n";
+       }
+    }
+   
+
+    //const pattern = document.getElementById("pattern")
+    pattern.innerHTML = pattern1();
+   
+
+
+
 }
-
 
 
 if ( message <= 1 || message >= 11) {
